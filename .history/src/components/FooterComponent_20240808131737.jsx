0663d9a0804d4c9d833e0690cc3a23be00c3282c
Abhilash2909 <abhilashabhi2909@gmail.com>
2024-08-24 07:53:13 +0530

    first commit

---
+++ .history/src/components/FooterComponent_20240808131737.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span className='text-muted'>All Rights Reserved 2024 by Abhilash</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import EmployeeComponent from './components/EmployeeComponent'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/* // http://localhost:3000 */}
+            <Route path='/' element = { <ListEmployeeComponent />}/>
+            {/* // http://localhost:3000/employees */}
+            <Route path='/employees' element = { <ListEmployeeComponent />}/>
+            {/* // http://localhost:3000/add-employee */}
+            <Route path='/add-employee' element = { <EmployeeComponent />}/>
+            {/* // http://localhost:3000/edit-employee */}
+            <Route path='/edit-employee/:id' element = { <EmployeeComponent />}/>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
